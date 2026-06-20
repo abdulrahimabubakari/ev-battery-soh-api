88597a30344a4051e5f68d7abd9d95280a1dfe57
abdulrahimabubakari <abdul.rahim.abubakari01@gmail.com>
2026-06-20 00:59:19 +0000

    feat: point frontend to live Render API

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import {
 } from "recharts";
 import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+const API = "https://battery-soh-predictor.onrender.com";
 
 function StatusBadge({ status }) {
   const colors = {
